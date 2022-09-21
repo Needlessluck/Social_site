e842bd4e546a160339636a05e51479f16da5934f
Needlessluck <shubh15shukla@gmail.com>
2022-09-22 01:10:02 +0530

    added the signup page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+import imp
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email Already Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already Taken')
+                return redirect('signup')
+            else:
+                user =User.objects.create_user(username=username,password=password,email=email)
+                user.save()
+
+                #log user in and direct to settings page
+
+                #create a profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model,id_user=user_model.id)
+                new_profile.save()
+                return redirect('signin')
+        else:
+            messages.info(request,'Password Not Matching')
+            return redirect('signup')
+    else:
+        return render(request,'signup.html')
+
+def signin(request):
+    return render(request,'signin.html')
